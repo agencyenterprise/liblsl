8162ecfdd368e461e375d5d47cbf829874dc0d4d
Chadwick Boulay <chadwick.boulay@gmail.com>
2022-11-05 22:45:02 -0400

    Fix index bug in SendData.cpp

--- examples/SendData.cpp
+++ examples/SendData.cpp
@@ -84,7 +84,6 @@ int main(int argc, char *argv[]) {
 		// send data forever
 		std::cout << "Now sending data... " << std::endl;
 		for (unsigned t = 0;; t++) {
-
 			// Create random data for the first 8 channels.
 			for (int c = 0; c < 8; c++) sample[c] = (float)((rand() % 1500) / 500.0 - 1.5);
 			// For the remaining channels, fill them with a sample counter (wraps at 1M).
@@ -99,7 +98,7 @@ int main(int argc, char *argv[]) {
 
 			// send the sample
 			if (contig) {
-				std::cout << sample[0] << "\t" << sample[8] << std::endl;
+				std::cout << sample[0] << "\t" << sample[n_channels-1] << std::endl;
 				outlet.push_sample(sample);
 			} else {
 				// Advanced: Push set of discontiguous buffers.
